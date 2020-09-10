a2015c71d3f5668f7344769395252da460782eff
laith <mr.laith_2011@hotmail.com>
2020-09-11 01:51:56 +0300

    finish Test

--- __test__/array-binary-search.test.js
+++ __test__/array-binary-search.test.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const binary = require('../challenges/arrayBinarySearch');
+const binary = require('../challenges/arrayBinarySearch/array-binary-search');
 
 
 describe('noteee', () => {
 
-    it('if the test pass', () => {
-        expect(binary([1, 2, 3, 5, 8], 2)).toEqual(2)
+  it('if the test pass', () => {
+    expect(binary([1, 2, 3, 5, 8], 2)).toEqual(2);
+    expect(binary([1, 2, 3, 5, 8], 7)).toEqual(-1);
 
-    });
+
+  });
 
 });
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function BinarySearch(storedArray, searchKey) {
-    var result;
-    storedArray.forEach(item => {
-        if (item == searchKey) {
-            result = item;
-        }
-    })
-    if (!result) {
-        result = -1;
+  var result;
+  storedArray.forEach(item => {
+    if (item == searchKey) {
+      result = item;
     }
-    return result;
+  });
+  if (!result) {
+    result = -1;
+  }
+  return result;
 }
 
 BinarySearch([1, 2, 3, 5, 8], 2);

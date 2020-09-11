be0d873f307844cbca2645f4f828ece2216cb874
laith <mr.laith_2011@hotmail.com>
2020-09-11 23:47:25 +0300

    finishtest1

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -1,71 +1,71 @@
 'use strict';
 
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+    constructor(value) {
+        this.value = value;
+        this.next = null;
 
-  }
+    }
 
 }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-    this.length = 0;
+    constructor() {
+        this.head = null;
+        this.length = 0;
 
-  }
-
-  insert(value) {
-    let newNode = new Node(value); // value: value, next: null
-    //if this linked list is empty 
-    console.log('1111', this.head);
-    if (!this.head) {
-      this.head = newNode;
-      this.length++;
-      return this;
-
-    } else {
-      // if we have other stuff, I need to add it at the end
-      // I have to loop through all nodes and add it to the tail
-      let curentNode = this.head;
-      while (curentNode.next) {
-        curentNode = curentNode.next;
-
-      }
-      curentNode.next = newNode;
-      this.length++;
-      console.log('22222', curentNode.next);
-      return this;
     }
-  }
 
-  includes(value) {
-    let curentNode = this.head;
-    while (curentNode) {
-      if (curentNode.value == value) {
-        console.log('exist');
-        return true;
-      }
-      curentNode = curentNode.next;
+    insert(value) {
+        let newNode = new Node(value); // value: value, next: null
+        //if this linked list is empty 
+        console.log('1111', this.head);
+        if (!this.head) {
+            this.head = newNode;
+            this.length++;
+            return this;
+
+        } else {
+            // if we have other stuff, I need to add it at the end
+            // I have to loop through all nodes and add it to the tail
+            let curentNode = this.head;
+            while (curentNode.next) {
+                curentNode = curentNode.next;
+
+            }
+            curentNode.next = newNode;
+            this.length++;
+            console.log('22222', curentNode.next);
+            return this;
+        }
     }
-    console.log('not exist');
-    return false;
 
-  }
+    includes(value) {
+        let curentNode = this.head;
+        while (curentNode) {
+            if (curentNode.value == value) {
+                console.log('exist');
+                return true;
+            }
+            curentNode = curentNode.next;
+        }
+        console.log('not exist');
+        return false;
 
-  toString() {
-    let string = '';
-    // "{ a } -> { b } -> { c } -> NULL"
-    // console.log('tt', this.head);
-    let curentNode = this.head;
-    while (curentNode) {
-      string = string + `{ ${curentNode.value}} -> `;
-      curentNode = curentNode.next;
     }
-    string = string + null;
-    return string;
-  }
+
+    toString() {
+        let string = '';
+        // "{ a } -> { b } -> { c } -> NULL"
+        // console.log('tt', this.head);
+        let curentNode = this.head;
+        while (curentNode) {
+            string = string + `{ ${curentNode.value}} -> `;
+            curentNode = curentNode.next;
+        }
+        string = string + null;
+        return string;
+    }
 
 }
 
@@ -86,3 +86,6 @@ console.log('+++++++++++++++', ll.toString());
 
 
 console.log('LL objects', ll);
+
+
+module.exports = LinkedList;

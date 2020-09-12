305c796be1a86a4a3630be198be03aa194f270cc
laith <mr.laith_2011@hotmail.com>
2020-09-13 00:53:11 +0300

    update

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -12,3 +12,37 @@ function reverse(array) {
 }
 
 console.log(reverse([1, 2, 3, 4, 5, 6, 7]));
+
+
+
+//another way 
+
+// function reverse(arr){
+//     var output = [];
+
+//     for (var i = arr.length -1 ; i >= 0  ; i--){
+//        output[output.length] = arr[i];
+//        console.log(arr[i])
+//     }
+
+//     return output;
+// }
+
+// console.log(reverse([1,2,3,4,5,6,7]));
+
+
+
+// another way
+
+// function reverse(arr){
+//     var output = [];
+//     for (var i = 0; i < arr.length ; i++){
+
+//        output[i] = arr[arr.length - i - 1];
+//        console.log(arr[arr.length -i - 1])
+//     }
+
+//     return output;
+// }
+
+// console.log(reverse([1,2,3,4,5,6,7]));

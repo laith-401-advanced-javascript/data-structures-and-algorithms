2912045f3ea8aa4deae1241a50b16f7ff67fd6d8
laith <mr.laith_2011@hotmail.com>
2020-10-16 21:40:03 +0300

    finishall

---
+++ challenges/hashtable/readme.md
@@ -0,0 +1,33 @@
+
+
+# data-structures-and-algorithms
+
+# hashTable
+Hash - A hash is the result of some algorithm taking an incoming string and converting it into a value that could be used for either security or some other purpose. In the case of a hashtable, it is used to determine the index of the array.
+Buckets - A bucket is what is contained in each index of the array of the hashtable. Each index is a bucket. An index could potentially contain multiple key/value pairs if a collision occurs.
+Collisions - A collision is what happens when more than one key gets hashed to the same location of the hashtable.
+
+## Challenge
+<!-- Description of the challenge -->
+we want to be able to search through the data to look up a neighborhood and obtain it’s zip code. We could do this using a for loop that looks through each piece of data one by one until it
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+this Challenge take from me 50 min  to do tha
+
+Big O  :
+
+Time  O(1) :  >> because we know   the index of the value in the array 
+Space : O(n) : >> because we make a new array 
+
+
+## Action Link 
+
+[ Action Link]
+
+(https://github.com/laith-401-advanced-javascript/data-structures-and-algorithms/actions/runs/243524335)
+
+## Solution
+<!-- Embedded whiteboard image -->
+
+![whiteboard](../../asset/hashTable.jpg)
--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -1,148 +1,148 @@
 'use strict';
 
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
 
-    }
+  }
 
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.length = 0;
 
+  }
+
+
+  insert(value) {
+    let newNode = new Node(value); // value: value, next: null
+    //if this linked list is empty 
+    // console.log('1111', this.head);
+    if (!this.head) {
+      this.head = newNode;
+      this.length++;
+      return this;
+
+    } else {
+      // if we have other stuff, I need to add it at the end
+      // I have to loop through all nodes and add it to the tail
+      let curentNode = this.head;
+      while (curentNode.next) {
+        curentNode = curentNode.next;
+      }
+      curentNode.next = newNode;
+      this.length++;
+      // console.log('22222', curentNode.next);
+      return this;
     }
+  }
 
+  // reverseLinkedList(value) {
+  //     let newNode = new Node(value);
+  //     let curentNode = this.head;
+  //     while (curentNode) {
+  //         curentNode.next = curentNode;
+  //     }
+  //     curentNode.next = newNode;
+  //     return this;
+  // }
 
-    insert(value) {
-        let newNode = new Node(value); // value: value, next: null
-        //if this linked list is empty 
-        // console.log('1111', this.head);
-        if (!this.head) {
-            this.head = newNode;
-            this.length++;
-            return this;
-
-        } else {
-            // if we have other stuff, I need to add it at the end
-            // I have to loop through all nodes and add it to the tail
-            let curentNode = this.head;
-            while (curentNode.next) {
-                curentNode = curentNode.next;
-            }
-            curentNode.next = newNode;
-            this.length++;
-            // console.log('22222', curentNode.next);
-            return this;
-        }
+  insertBefore(value, newValue) {
+    let curentNode = this.head;
+    while (curentNode.next.value !== value) {
+      curentNode = curentNode.next;
     }
+    let newNode = new Node(newValue);
 
-    // reverseLinkedList(value) {
-    //     let newNode = new Node(value);
-    //     let curentNode = this.head;
-    //     while (curentNode) {
-    //         curentNode.next = curentNode;
-    //     }
-    //     curentNode.next = newNode;
-    //     return this;
-    // }
+    newNode.next = curentNode.next;
+    curentNode.next = newNode;
+    this.length++;
+    return this;
 
-    insertBefore(value, newValue) {
-        let curentNode = this.head;
-        while (curentNode.next.value !== value) {
-            curentNode = curentNode.next;
-        }
-        let newNode = new Node(newValue);
+  }
 
-        newNode.next = curentNode.next;
-        curentNode.next = newNode;
-        this.length++;
-        return this;
 
+  insertAfter(value, newValue) {
+    let curentNode = this.head;
+    while (curentNode.value !== value) {
+      curentNode = curentNode.next;
     }
+    let newNode = new Node(newValue);
 
+    newNode.next = curentNode.next;
+    curentNode.next = newNode;
+    this.length++;
+    return this;
 
-    insertAfter(value, newValue) {
-        let curentNode = this.head;
-        while (curentNode.value !== value) {
-            curentNode = curentNode.next;
-        }
-        let newNode = new Node(newValue);
+  }
 
-        newNode.next = curentNode.next;
-        curentNode.next = newNode;
-        this.length++;
-        return this;
-
+  kthFromEnd(k) {
+    let curentNode = this.head;
+    let length = 0;
+    while (curentNode) {
+      length++;
+      curentNode = curentNode.next;
     }
-
-    kthFromEnd(k) {
-        let curentNode = this.head;
-        let length = 0;
-        while (curentNode) {
-            length++;
-            curentNode = curentNode.next;
-        }
-        let requireNode = length - 1 - k;
-        if (requireNode < 0 || k < 0) {
-            return 'not valid';
-        }
-        curentNode = this.head;
-        while (requireNode > 0) {
-            requireNode--;
-            curentNode = curentNode.next;
-        }
-        return curentNode.value;
+    let requireNode = length - 1 - k;
+    if (requireNode < 0 || k < 0) {
+      return 'not valid';
     }
-
-    includes(value) {
-        let curentNode = this.head;
-        //
-        while (curentNode) {
-            if (curentNode.value == value) {
-                // console.log('exist');
-                return true;
-            }
-            curentNode = curentNode.next;
-        }
-        // console.log('not exist');
-        return false;
-
+    curentNode = this.head;
+    while (requireNode > 0) {
+      requireNode--;
+      curentNode = curentNode.next;
     }
+    return curentNode.value;
+  }
 
-    toString() {
-        let string = '';
-        // "{ a } -> { b } -> { c } -> NULL"
-        // console.log('tt', this.head);
-        let curentNode = this.head;
-        while (curentNode) {
-            string = string + `{ ${curentNode.value}} -> `;
-            curentNode = curentNode.next;
-        }
-        string = string + null;
-        return string;
+  includes(value) {
+    let curentNode = this.head;
+    //
+    while (curentNode) {
+      if (curentNode.value == value) {
+        // console.log('exist');
+        return true;
+      }
+      curentNode = curentNode.next;
     }
+    // console.log('not exist');
+    return false;
 
-    mergeLists(list1, list2) {
-        let currentNode1 = list1.head;
-        let currentNode2 = list2.head;
+  }
 
-        while (currentNode1 || currentNode2) {
-            if (currentNode1) {
-                this.insert(currentNode1.value);
-                currentNode1 = currentNode1.next;
-            }
-            if (currentNode2) {
-                this.insert(currentNode2.value);
-                currentNode2 = currentNode2.next;
-            }
-        }
-        return this;
-
+  toString() {
+    let string = '';
+    // "{ a } -> { b } -> { c } -> NULL"
+    // console.log('tt', this.head);
+    let curentNode = this.head;
+    while (curentNode) {
+      string = string + `{ ${curentNode.value}} -> `;
+      curentNode = curentNode.next;
     }
+    string = string + null;
+    return string;
+  }
+
+  mergeLists(list1, list2) {
+    let currentNode1 = list1.head;
+    let currentNode2 = list2.head;
+
+    while (currentNode1 || currentNode2) {
+      if (currentNode1) {
+        this.insert(currentNode1.value);
+        currentNode1 = currentNode1.next;
+      }
+      if (currentNode2) {
+        this.insert(currentNode2.value);
+        currentNode2 = currentNode2.next;
+      }
+    }
+    return this;
+
+  }
 
 
 
@@ -150,20 +150,20 @@ class LinkedList {
 
 function reverseLinkedList(list1) {
 
-    let curentNode1 = list1.head;
-    if (!curentNode1) {
-        return `linked list is empty`;
-    }
-    let arr = [];
-    while (curentNode1) {
-        arr.push(curentNode1.value);
-        curentNode1 = curentNode1.next;
-    }
-    let newLL = new LinkedList();
-    for (let i = arr.length - 1; i > -1; i--) {
-        newLL.insert(arr[i]);
-    }
-    return newLL;
+  let curentNode1 = list1.head;
+  if (!curentNode1) {
+    return `linked list is empty`;
+  }
+  let arr = [];
+  while (curentNode1) {
+    arr.push(curentNode1.value);
+    curentNode1 = curentNode1.next;
+  }
+  let newLL = new LinkedList();
+  for (let i = arr.length - 1; i > -1; i--) {
+    newLL.insert(arr[i]);
+  }
+  return newLL;
 
 }
 
@@ -210,7 +210,7 @@ console.log('-------', ll.toString());
 console.log('-------', ll3.mergeLists(ll, ll2).toString());
 // console.log('-------', llpalindrom(ll).toString());
 
-// console.log('-------', reverseLinkedList(ll).toString());
+console.log('-------', reverseLinkedList(ll).toString());
 // console.log('LL objects', ll);
 
 // ll3.reverseLinkedList(ll2);
